91970f684a20d503b7bab60ebe5b1d9b5afc0d91
Kauã Salomão <kauazaca@gmail.com>
2024-11-04 19:58:23 -0300

    Add files via upload

---
+++ DDL/est_caso2sql.sql
@@ -0,0 +1,76 @@
+use Est_Caso_2;
+
+CREATE TABLE IF NOT EXISTS Voos(
+    Voo_Codigo INT PRIMARY KEY,
+    data_Hora_Origem DATETIME NOT NULL,
+    data_Hora_Destino DATETIME NOT NULL,
+    aeroporto_Origem VARCHAR(50) NOT NULL,
+    aeroporto_Destino VARCHAR(50) NOT NULL,
+    aeronave_Prefixo VARCHAR(5),
+    CONSTRAINT Aeronave_FK FOREIGN KEY(Aeronave_Prefixo)
+    REFERENCES Aeronaves(aeronave_Prefixo)
+);
+CREATE TABLE IF NOT EXISTS Aeronaves(
+    aeronave_Prefixo VARCHAR(10) PRIMARY KEY,
+    aeronave_Modelo VARCHAR(45) NOT NULL,
+    aeronave_anofabri VARCHAR(45) NOT NULL,
+    aeronave_capacidade VARCHAR(45) NOT NULL,
+    aeronave_Fabricante VARCHAR(45) NOT NULL
+);
+ALTER TABLE Aeronaves
+CHANGE COLUMN aeronave_anofabri aeronave_fabriano VARCHAR(50);
+CREATE TABLE IF NOT EXISTS Aeroportos(
+    aeroporto_Cod VARCHAR(3) PRIMARY KEY,
+    aeroporto_Nome VARCHAR(45) NOT NULL,
+    aeroporto_Local VARCHAR(50) NOT NULL,
+    aeroporto_Pais VARCHAR(50) NOT NULL,
+    aeroporto_Longitude FLOAT NOT NULL,
+    aeroporto_Latitude FLOAT NOT NULL
+);
+ALTER TABLE Aeroportos
+MODIFY COLUMN aeroporto_Cod VARCHAR(5);
+CREATE TABLE IF NOT EXISTS Reservas(
+    FK_Voo INT,
+    FK_Pass VARCHAR(11),
+    CONSTRAINT voo_fk FOREIGN KEY (FK_Voo)
+    REFERENCES Voos(Voo_Codigo),
+    CONSTRAINT passageros_fk FOREIGN KEY(FK_Pass)
+    REFERENCES Passageiros(pass_CPF)
+);
+
+ALTER TABLE Reservas
+ADD COLUMN Assento VARCHAR(2);
+
+ALTER TABLE Reservas
+DROP COLUMN Assento;
+
+CREATE TABLE IF NOT EXISTS Passageiros(
+    pass_CPF VARCHAR(11) PRIMARY KEY,
+    pass_Nome VARCHAR(45) NOT NULL,
+    pass_Tel VARCHAR(13) NOT NULL,
+    pass_End VARCHAR(100) NOT NULL,
+    pass_Email VARCHAR(45) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Passageiros_II(
+    pass_CPF VARCHAR(11) PRIMARY KEY,
+    pass_Nome VARCHAR(45) NOT NULL,
+    pass_Tel VARCHAR(13) NOT NULL,
+    pass_End VARCHAR(100) NOT NULL,
+    pass_Email VARCHAR(45) NOT NULL
+);
+DROP TABLE Passageiros_II;
+CREATE TABLE IF NOT EXISTS Funcionarios(
+    func_ID INT PRIMARY KEY,
+    func_Funcao VARCHAR(30) NOT NULL,
+    func_Nome VARCHAR(45) NOT NULL,
+    func_Data_Nasc DATE NOT NULL,
+    func_Tel VARCHAR(13) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Operam_Voo(
+    Func_FK INT,
+    Voos_FK INT,
+    CONSTRAINT funcionarios_FK FOREIGN KEY(Func_FK)
+    REFERENCES Funcionarios(func_ID),
+    CONSTRAINT voos_FK FOREIGN KEY(Voos_FK)
+    REFERENCES Voos(Voo_Codigo)
+);
---
+++ DDL/est_caso5sql.sql
@@ -0,0 +1,100 @@
+Use Est_Caso_5;
+
+CREATE TABLE IF NOT EXISTS Clientes(
+    CPF VARCHAR(11) PRIMARY KEY,
+    Nome_Clientes VARCHAR(75) NOT NULL,
+    Data_Nasc_Clientes DATE NOT NULL,
+    Status_Fideli_Cliente ENUM('Normal','Prata','Ouro'),
+    Cod_Pagamentos VARCHAR(50),
+    CONSTRAINT FK_Pagamento_Clientes FOREIGN KEY(Cod_Pagamentos)
+    REFERENCES Pagamentos(Cod_Pagamentos)
+);
+
+CREATE TABLE IF NOT EXISTS Pagamentos(
+    Cod_Pagamentos VARCHAR(50) PRIMARY KEY,
+    Data_Pagamentos DATE NOT NULL,
+    Valor_Pagamentos FLOAT NOT NULL,
+    Status_Pagamento ENUM('PENDENTE','CONCLUÍDO','EM ESPERA'),
+    Cod_Venda VARCHAR(50),
+    CONSTRAINT FK_Vendas_Pag FOREIGN KEY (Cod_Venda)
+    REFERENCES Vendas(Cod_Venda)
+);
+
+CREATE TABLE IF NOT EXISTS Vendas(
+    Cod_Venda VARCHAR(50) PRIMARY KEY,
+    Nome_comprados VARCHAR(50) NOT NULL,
+    QTD_Comprados INT not null,
+    Cod_Pagamentos VARCHAR(50),
+    CONSTRAINT FK_Pag_Vendas FOREIGN KEY (Cod_Pagamentos)
+    REFERENCES Pagamentos(Cod_Pagamentos)
+);
+
+CREATE TABLE IF NOT EXISTS Produtos(
+    Cod_Prod VARCHAR(30) PRIMARY KEY,
+    Estoque VARCHAR(100) NOT NULL,
+    Preco FLOAT NOT NULL,
+    Categoria VARCHAR(30)
+);
+ALTER TABLE Produtos
+CHANGE COLUMN Categoria Categoria_Produtos VARCHAR(50);
+
+ALTER TABLE Produtos
+CHANGE COLUMN Categoria_Produtos Categoria VARCHAR(30);
+
+CREATE TABLE IF NOT EXISTS Fornecedores(
+    Cod_Fornecedores VARCHAR(30) PRIMARY KEY,
+    Contato_Fornecedor VARCHAR(50) NOT NULL,
+    Produtos_Fornecidos VARCHAR(50) NOT NULL,
+    Cod_Prod VARCHAR(30),
+    CONSTRAINT FK_Prod_Forn FOREIGN KEY (Cod_Prod)
+    REFERENCES Produtos(Cod_Prod)
+);
+
+ALTER TABLE Fornecedores
+ADD COLUMN Data_Fornecedores DATE;
+
+
+ALTER TABLE Fornecedores
+DROP COLUMN Data_Fornecedores;
+
+CREATE TABLE IF NOT EXISTS Enderecos(
+    ID_Enderecos INT PRIMARY KEY,
+    Num_End INT NOT NULL,
+    Tipo_End VARCHAR(20) NOT NULL,
+    Logradouro_End VARCHAR(50) NOT NULL,
+    Complemento VARCHAR(100),
+    CEP_End INT NOT NULL,
+    Bairro_End VARCHAR(50) NOT NULL,
+    Estado_End VARCHAR(30) NOT NULL,
+    Cidade_End VARCHAR(50) NOT NULL,
+    CPF VARCHAR(11),
+    CONSTRAINT FK_Clientes_End FOREIGN KEY(CPF)
+    REFERENCES Clientes(CPF)
+    );
+
+CREATE TABLE IF NOT EXISTS Clientes_Produtos(
+    CPF VARCHAR(11),
+    Cod_Prod VARCHAR(30),
+    PRIMARY KEY(CPF, Cod_Prod),
+    CONSTRAINT FK_Clientes_Produtos FOREIGN KEY(CPF)
+    REFERENCES Clientes(CPF),
+    CONSTRAINT FK_Produtos_Clientes FOREIGN KEY (Cod_Prod)
+    REFERENCES Produtos(Cod_Prod)
+);
+ALTER TABLE Clientes_Produtos
+MODIFY COLUMN Cod_Prod VARCHAR(100);
+
+ALTER TABLE Clientes_Produtos
+MODIFY COLUMN Cod_Prod VARCHAR(30);
+
+CREATE TABLE IF NOT EXISTS Vendas_Produtos(
+    Cod_Venda VARCHAR(50),
+    Cod_Prod VARCHAR(30),
+    PRIMARY KEY(Cod_Venda, Cod_Prod),
+    CONSTRAINT FK_Vendas_Produtos FOREIGN KEY(Cod_Venda)
+    REFERENCES Vendas(Cod_Venda),
+    CONSTRAINT FK_Produtos_Vendas FOREIGN KEY (Cod_Prod)
+    REFERENCES Produtos(Cod_Prod)
+);
+
+DROP TABLE Vendas_Produtos;
